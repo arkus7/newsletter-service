6376fe7f6e79b670fd8232ad5dcac3584c9f80cf
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-06 14:48:46 +0100

    Add tests for subscription endpoint

---
+++ tests/subscriptions.rs
@@ -0,0 +1,46 @@
+mod test_helpers;
+
+#[actix_rt::test]
+async fn subscribe_returns_200_ok_for_valid_form_data() {
+    let address = test_helpers::spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursule_le_guin%40.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-ww-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16())
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_bad_request_when_data_is_missing() {
+    let address = test_helpers::spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-ww-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
+}

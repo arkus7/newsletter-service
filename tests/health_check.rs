e9314593e34fc85798d8f7a58415ef6853c71eb1
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-06 14:41:13 +0100

    Move `spawn_app` helper function to separate file

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,8 @@
-use std::net::TcpListener;
+mod test_helpers;
 
 #[actix_rt::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let address = test_helpers::spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
@@ -13,16 +13,4 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(0, response.content_length().unwrap());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = newsletter_service::run(listener).expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{}", port)
 }
---
+++ tests/test_helpers.rs
@@ -0,0 +1,13 @@
+use std::net::TcpListener;
+
+pub fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter_service::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}

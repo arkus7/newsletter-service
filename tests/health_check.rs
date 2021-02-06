c9dfaf928cadf3bfa44e2850a74ef7c9a9aa12da
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-06 13:50:47 +0100

    Make tests run on a random port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,12 @@
+use std::net::TcpListener;
+
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -14,10 +15,14 @@ async fn health_check_works() {
     assert_eq!(0, response.content_length().unwrap());
 }
 
-fn spawn_app() {
-    let server = newsletter_service::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter_service::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }

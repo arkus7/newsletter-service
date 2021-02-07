edb1d0813304dc055a28fc4f1c8bf480e641c809
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-07 11:39:51 +0100

    Remove unnecessary default value for variable in wait for db script

--- scripts/wait_for_db.sh
+++ scripts/wait_for_db.sh
@@ -11,7 +11,7 @@ export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/$
 
 docker-compose -f docker-compose.yml up -d postgres
 
-export PGPASSWORD="${DB_PASSWORD:=password}"
+export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "${DB_NAME}" -c '\q'; do
   >&2 echo "Postgres is still unavailable - sleeping"
   sleep 1

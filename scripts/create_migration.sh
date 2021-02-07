56b850c87a4fccecbf9787b4a6f4cfa38b22d54e
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-07 11:36:25 +0100

    Wait for database before running scripts on database

--- scripts/create_migration.sh
+++ scripts/create_migration.sh
@@ -9,21 +9,8 @@ MIGRATION_NAME=$1
   exit 1
 }
 
-DB_USER="${POSTGRES_USER:=postgres}"
-DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-DB_NAME="${POSTGRES_DB:=newsletter}"
-DB_HOST="${POSTGRES_HOST:=localhost}"
-DB_PORT="${POSTGRES_PORT:=5432}"
+SCRIPTS_DIR=$(dirname "$0")
 
-docker-compose -f docker-compose.yml up -d postgres
+. "${SCRIPTS_DIR}/wait_for_db.sh"
 
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "${DB_NAME}" -c '\q'; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
-
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 sqlx migrate add "${MIGRATION_NAME}"

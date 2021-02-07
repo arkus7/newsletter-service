04501407cd8142fc669ed68820bf26790eb07131
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-07 11:36:11 +0100

    Add `wait_for_db` script

---
+++ scripts/wait_for_db.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+
+docker-compose -f docker-compose.yml up -d postgres
+
+export PGPASSWORD="${DB_PASSWORD:=password}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "${DB_NAME}" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"

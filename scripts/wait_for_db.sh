2a8b05cacf03e5e8f18f69262354b3962b1e7729
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-07 11:45:11 +0100

    Allow to skip launching database in docker in `wait_for_db` script

--- scripts/wait_for_db.sh
+++ scripts/wait_for_db.sh
@@ -9,7 +9,9 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 
-docker-compose -f docker-compose.yml up -d postgres
+[[ -z "${SKIP_DOCKER}" ]] && {
+  docker-compose -f docker-compose.yml up -d postgres
+}
 
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "${DB_NAME}" -c '\q'; do

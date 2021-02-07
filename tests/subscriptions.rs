94e97826923fbf9e1279d788085d12ca63ab2e19
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-07 12:34:03 +0100

    Add test checking if subscription was saved to the database

--- tests/subscriptions.rs
+++ tests/subscriptions.rs
@@ -1,8 +1,12 @@
+use newsletter_service::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
+
 mod test_helpers;
 
 #[actix_rt::test]
 async fn subscribe_returns_200_ok_for_valid_form_data() {
     let address = test_helpers::spawn_app();
+
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -15,6 +19,38 @@ async fn subscribe_returns_200_ok_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16())
+}
+
+#[actix_rt::test]
+async fn subscribe_stores_subscription_in_database() {
+    let config = get_configuration().expect("Failed to read configuration");
+    let conn_string = config.database.connection_string();
+    let mut connection = PgConnection::connect(&conn_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = test_helpers::spawn_app();
+
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]

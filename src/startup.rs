6ca042ce98a6b78eefa8f79db5c90071bf64044e
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-09 21:14:18 +0100

    Add PgPool as app data

--- src/startup.rs
+++ src/startup.rs
@@ -3,12 +3,14 @@ use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
+use sqlx::PgPool;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();

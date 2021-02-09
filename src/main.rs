60bc606d539af84dac710690cb4640ffc7d49ce5
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-02-09 21:14:45 +0100

    Initialize db connection pool and pass it to run method

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use newsletter_service::configuration::get_configuration;
 use newsletter_service::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to the database");
+
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address).expect(&format!(
         "Failed to bind to port {}",
         config.application_port
     ));
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }

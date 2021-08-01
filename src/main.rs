28216d333dfbf88715af5164c32bb3e6640fb91e
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-01 15:28:27 +0200

    Setup tracing logger

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use env_logger::Env;
 use newsletter_service::configuration::get_configuration;
 use newsletter_service::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::{subscriber::set_global_default, Subscriber};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let logs_subscriber = get_logs_subscriber("newsletter_service".into(), "info".into());
+    init_logs_subscriber(logs_subscriber);
 
     let config = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&config.database.connection_string())
@@ -16,4 +20,21 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to port");
     run(listener, connection_pool)?.await
+}
+
+pub fn get_logs_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_logs_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
 }

4a538c57730ce2cfe92a2e92fc7f572fa414d915
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-01 15:34:13 +0200

    Move telemetry setup to separate mod

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use newsletter_service::configuration::get_configuration;
 use newsletter_service::startup::run;
+use newsletter_service::telemetry::{get_logs_subscriber, init_logs_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::{subscriber::set_global_default, Subscriber};
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -20,21 +17,4 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to port");
     run(listener, connection_pool)?.await
-}
-
-pub fn get_logs_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
-
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-pub fn init_logs_subscriber(subscriber: impl Subscriber + Send + Sync) {
-    LogTracer::init().expect("Failed to set logger");
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
 }
